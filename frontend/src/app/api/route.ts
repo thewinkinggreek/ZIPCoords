824a38eb9b91e4cca6cd4acac9b761f7b6fa4028
mikezangus <zangusm@gmail.com>
2025-04-19 13:28:25 -0400

    fixed supabase queries

--- frontend/src/app/api/route.ts
+++ frontend/src/app/api/route.ts
@@ -17,10 +17,12 @@ async function getCoords(
         .select("location")
         .eq("zip", input.value)
         .single();
+    console.log("Data:\n", data)
     if (error || !data || !data.location || !Array.isArray(data.location.coordinates)) {
         return null;
     }
     const [lon, lat] = data.location.coordinates;
+    console.log("Lat and lon:\n", lat, lon)
     return { lat, lon };
 }
 
@@ -57,7 +59,9 @@ export async function GET(req: NextRequest)
     }
     const input = parseInput(query);
     if (input.type === InputType.zip) {
+        console.log("Input:\n", input)
         const result = await getCoords(input);
+        console.log("Result:\n", result)
         return result
             ? NextResponse.json(result)
             : NextResponse.json({ error: "ZIP Code not found" }, { status: 404 });
