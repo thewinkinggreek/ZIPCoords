c392574ea6a976ee39d5dc8b3eda39a4bfb4e7b4
mikezangus <zangusm@gmail.com>
2025-04-20 21:53:30 -0400

    no submit button - check input regex on every character change, fetch if match

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import fetchResponse from "@/lib/fetchResponse";
-import { Response } from "@/types";
+import { isZIPCodeOrCoords } from "@/lib/regexes";
+import { Response } from "@/lib/types";
+import styles from "@/styles/Fields.module.css";
 
 
 export default function Input(
@@ -12,25 +14,36 @@ export default function Input(
 )
 {
     const [input, setInput] = useState("");
-    async function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        setError(null);
-        setResult(null);
-        const { response, error } = await fetchResponse(input);
+    async function fetchResult(input: string)
+    {
+        const { response, error } =  await fetchResponse(input);
         if (error) {
+            setResult(null);
             setError(error);
         } else {
             setResult(response);
+            setError(null);
+        }
+    }
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>)
+    {
+        const val = e.target.value;
+        setInput(val);
+        if (isZIPCodeOrCoords(val)) {
+            fetchResult(val);
+        } else {
+            setResult(null);
+            setError(null);
         }
     }
     return (
-        <form onSubmit={handleSubmit}>
+        <div className={styles.fieldContainer}>
             <input
+                className={styles.textContainer}
                 value={input}
-                onChange={e => setInput(e.target.value)}
-                placeholder="Enter"
+                onChange={handleChange}
+                placeholder="Enter a ZIP Code or a set of Coordinates"
             />
-            <button type="submit">Search</button>
-        </form>
+        </div>
     );
 }
