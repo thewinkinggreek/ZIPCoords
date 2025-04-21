e955d39358d35478ed68703f322f257b472cc240
mikezangus <zangusm@gmail.com>
2025-04-20 23:16:08 -0400

    API guide

--- frontend/src/app/api-guide/page.tsx
+++ frontend/src/app/api-guide/page.tsx
@@ -1,6 +1,45 @@
+import styles from "@/styles/API.module.css";
+
+
 export default function APIGuidePage()
 {
     return (
-        <h1>API Guide</h1>
+        <div className={styles.container}>
+            <h1>ZIPCoords API Guide</h1>
+            <div className={styles.section}>
+                <span className={styles.bold_underline}>URL Structure:</span>
+                <span className={styles.code}>{"zipcoords.us/api?q={input}"}</span>
+            </div>
+            <div className={styles.section}>
+                <div className={styles.bold_underline}>Example: Get coordinates via ZIP Code</div>
+                <div>
+                    <span className={styles.bold}>ZIP Code:</span>
+                    <span className={styles.code}> 10001</span>
+                </div>
+                <div>
+                    <span className={styles.bold}>URL:</span>
+                    <span className={styles.code}> zipcoords.us/api?q=10001</span>
+                </div>
+                <div>
+                    <span className={styles.bold}>JSON response:</span>
+                    <span className={styles.code}> {'{"lat":40.7536854,"lon":-73.9991637}'}</span> 
+                </div>
+            </div>
+            <div className={styles.section}>
+                <div className={styles.bold_underline}>Example: Get ZIP Code via coordinates</div>
+                <div>
+                    <span className={styles.bold}>Coordinates:</span>
+                    <span className={styles.code}> 34.1030032, -118.4104684</span>
+                </div>
+                <div>
+                    <span className={styles.bold}>URL:</span>
+                    <span className={styles.code}> zipcoords.us/api?q=34.1030032,-118.4104684</span>
+                </div>
+                <div>
+                    <span className={styles.bold}>JSON response:</span>
+                    <span className={styles.code}> {'{"zip_code":90210}'}</span> 
+                </div>
+            </div>
+        </div>
     );
 }
