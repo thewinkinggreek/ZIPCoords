7a002cf3bc8676c7b3df5f10ca1b9cdd6f2670f5
mikezangus <zangusm@gmail.com>
2025-04-23 01:02:50 -0400

    hamburger menu on mobile

--- site/src/components/Header.tsx
+++ site/src/components/Header.tsx
@@ -1,22 +1,35 @@
+"use client";
+
+
 import Link from "next/link";
 import styles from "@/styles/Header.module.css";
+import { useState } from "react";
 
 
 export default function Header()
 {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={styles.headerContainer}>
             <div className={styles.navBarContainer}>
-                <Link href='/' className={styles.titleContainer}>
+                <Link
+                    href='/'
+                    className={styles.titleContainer}
+                >
                     ZIPCoords
                 </Link>
-                <div className={styles.linksContainer}>
-                    <Link href='/'>
-                        Home
-                    </Link>
-                    <Link href="/api-guide">
-                        API
-                    </Link>
+                <button
+                    className={styles.hamburger}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    ☰
+                </button>
+                <div className={
+                    `${styles.linksContainer} ${isOpen ? styles.showMenu : ""}`
+                }>
+                    <Link href='/'>Home</Link>
+                    <Link href="/packages">Packages</Link>
+                    <Link href="/api-guide">API</Link>
                 </div>
             </div>
         </div>
