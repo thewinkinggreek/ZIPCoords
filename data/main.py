121a70f1bb6ab82f116e307dc6095a645df3b885
mikezangus <zangusm@gmail.com>
2025-04-23 23:23:21 -0400

    only 1 coords source

--- data/main.py
+++ data/main.py
@@ -1,19 +1,17 @@
-import argparse
 import os
 import requests
 from dotenv import load_dotenv
 
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-GOOGLE_OUTPUT_PATH = os.path.join(CURRENT_DIR, "data.csv")
-OSM_OUTPUT_PATH = os.path.join(CURRENT_DIR, "data_osm.csv")
+OUTPUT_PATH = os.path.join(CURRENT_DIR, "data.csv")
 
 
 def generate_zip_codes() -> list[str]:
     return [f"{i:05d}" for i in range (500, 100000)]
 
 
-def fetch_coords_from_google(
+def fetch_coords(
     zip_code: str, session: requests.Session
 ) -> tuple[float | None, float | None]:
     load_dotenv(os.path.join(CURRENT_DIR, ".env.local"))
@@ -49,29 +47,6 @@ def fetch_coords_from_google(
         print(f"Error fetching coordinates from Google Maps for {zip_code}:\n", e)
     print(f"{zip_code} ❌")
     return None, None
-            
-
-def fetch_coords_from_osm(
-    zip_code: str, session: requests.Session
-) -> tuple[float | None, float | None]:
-    url = "https://nominatim.openstreetmap.org/search"
-    params = {
-        "postalcode": zip_code,
-        "countrycodes": "us",
-        "format": "json",
-        "limit": 1
-    }
-    try:
-        response = session.get(url, params=params, timeout=60)
-        response.raise_for_status()
-        data = response.json()
-        if data:
-            print(f"{zip_code} ✅")
-            return float(data[0]["lat"]), float(data[0]["lon"])
-    except Exception as e:
-        print(f"Error fetching coordinates from OSM for {zip_code}:\n", e)
-    print(f"{zip_code} ❌")
-    return None, None
 
 
 def write_to_csv(path: str, zip_code: str, lat: float, lon: float) -> None:
@@ -87,28 +62,12 @@ def write_to_csv(path: str, zip_code: str, lat: float, lon: float) -> None:
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--source",
-        choices=["google", "osm"],
-        default="osm",
-        help="Data source for coordinates"
-    )
-    args = parser.parse_args()
-    if args.source == "google":
-        print("Fetching coordinates from Google Maps")
-        fetch = fetch_coords_from_google
-        path = GOOGLE_OUTPUT_PATH
-    else:
-        print("Fetching coordinates from Open Street Map")
-        fetch = fetch_coords_from_osm
-        path = OSM_OUTPUT_PATH
     zip_codes = generate_zip_codes()
     session = requests.session()
     for zip_code in zip_codes:
-        lat, lon = fetch(zip_code, session)
+        lat, lon = fetch_coords(zip_code, session)
         if lat is not None and lon is not None:
-            write_to_csv(path, zip_code, lat, lon)
+            write_to_csv(OUTPUT_PATH, zip_code, lat, lon)
     
 
 if __name__ == "__main__":
