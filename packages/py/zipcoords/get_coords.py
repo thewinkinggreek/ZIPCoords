7b38724dd4be1ea8f5eb7003d866eaf44bd4a530
mikezangus <zangusm@gmail.com>
2025-04-25 20:47:42 -0400

    works with int input zip

--- packages/py/zipcoords/get_coords.py
+++ packages/py/zipcoords/get_coords.py
@@ -2,20 +2,19 @@ import json
 from importlib.resources import files
 
 
-def zip_to_coords(zip: str) -> tuple[float, float]:
+def get_coords(zip: str | int) -> tuple[float, float]:
+    if isinstance(zip, int):
+        zip = f"{zip:05d}"
     if (
         not isinstance(zip, str)
         or not zip.isdigit()
         or len(zip) != 5
     ):
         return None, None
-
     path = files("zipcoords.data").joinpath("data_dict.json")
     with open(path, 'r') as f:
         data: dict[str, list[float, float]] = json.load(f)
-
     coords = data.get(zip)
     if not coords:
         return None, None
-
     return coords[0], coords[1]
